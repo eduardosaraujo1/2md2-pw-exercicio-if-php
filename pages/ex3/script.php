74a8ebe8f7042db9250c6997f3e7e3f60740e02e
eduardosaraujo1 <eduardo.araujo109@etec.sp.gov.br>
2024-04-21 22:40:39 -0300

    Estilizado pag inicial ex 2

--- pages/ex3/script.php
+++ pages/ex3/script.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Atividades em PHP</title>
+    <link rel="stylesheet" href="css/result.css">
 </head>
+
 <body>
-    <?php 
-        echo "Hello World!"
+    <?php
+    echo "Hello World!"
     ?>
 </body>
+
 </html>
