a26a0e006990cb046c8eb9b37a2f067cb0f6b0d8
eduardosaraujo1 <eduardo.araujo109@etec.sp.gov.br>
2024-04-22 00:28:40 -0300

    Finalizado ex2

--- pages/ex2/script.php
+++ pages/ex2/script.php
@@ -11,22 +11,76 @@
 
 <body>
     <?php
-    $peso = $_GET["peso"];
-    $altura = $_GET["altura"];
+    $peso = $_POST["peso"];
+    $altura = $_POST["altura"];
 
     function calcularIMC($peso, $altura)
     {
-        return $peso / ($altura * $altura);
+        if ($altura > 30) {
+            // Assuma o usuário erroniamente digitou a altura em centimetros
+            $altura /= 100;
+        }
+        $imc = $peso / ($altura * $altura);
+        return round($imc, 1);
+    }
+
+    function classificarIMC($imc)
+    {
+        $class = "";
+        if ($imc < 18.5) {
+            $class = "Abaixo do peso normal";
+        } elseif (18.5 <= $imc && $imc < 25) {
+            $class = "Peso normal";
+        } elseif (25 < $imc && $imc < 30) {
+            $class = "Excesso de peso";
+        } elseif (30 < $imc && $imc < 35) {
+            $class = "Excesso de peso";
+        } elseif (35 < $imc && $imc < 40) {
+            $class = "Excesso de peso";
+        } elseif ($imc >= 40) {
+            $class = "Obesidade classe III";
+        }
+        return $class;
+    }
+
+    function classificarIMCID($imc)
+    {
+        $class = "";
+        if ($imc < 18.5) {
+            $class = "imc-abaixo-normal";
+        } elseif (18.5 <= $imc && $imc < 25) {
+            $class = "imc-normal";
+        } elseif (25 < $imc && $imc < 30) {
+            $class = "imc-excesso";
+        } elseif (30 < $imc && $imc < 35) {
+            $class = "imc-obesidadeI";
+        } elseif (35 < $imc && $imc < 40) {
+            $class = "imc-obesidadeII";
+        } elseif ($imc >= 40) {
+            $class = "imc-obesidadeIII";
+        }
+        return $class;
     }
     ?>
     <div class="container">
-        <span class="title">Seu IMC é...</span>
-        <div class="header">
-            <?php
-            $imc = calcularIMC($peso, $altura);
-            echo number_format($imc, 1, ",", "");
-            ?>
+        <div class="dados">
+            <span class="title">Seu IMC é</span>
+            <div class="imc-calculado">
+                <?php
+                $imc = calcularIMC($peso, $altura);
+                echo number_format($imc, 1, ",", "");
+                ?>
+            </div>
+            <span class="descricao">Sua classificação é</span>
+            <span class="imc-classificacao">
+                <?php
+                $class = classificarIMC($imc);
+                $classId = classificarIMCID($imc);
+                echo $class;
+                ?>
+            </span>
         </div>
+
         <table class="table">
             <thead>
                 <tr>
@@ -35,36 +89,44 @@
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td class="imc-abaixo-normal">Menor que 18,5</td>
-                    <td class="imc-abaixo-normal">Abaixo do peso normal</td>
+                <tr id="imc-abaixo-normal">
+                    <td>Menor que 18,5</td>
+                    <td>Abaixo do peso normal</td>
                 </tr>
-                <tr>
-                    <td class="imc-normal">18,5 - 24,9</td>
-                    <td class="imc-normal">Peso normal</td>
+                <tr id="imc-normal">
+                    <td>18,5 - 24,9</td>
+                    <td>Peso normal</td>
                 </tr>
-                <tr>
-                    <td class="imc-excesso">25,0 - 29,9</td>
-                    <td class="imc-excesso">Excesso de peso</td>
+                <tr id="imc-excesso">
+                    <td>25,0 - 29,9</td>
+                    <td>Excesso de peso</td>
                 </tr>
-                <tr>
-                    <td class="imc-obesidade1">30,0 - 34,9</td>
-                    <td class="imc-obesidade1">Obesidade classe I</td>
+                <tr id="imc-obesidadeI">
+                    <td>30,0 - 34,9</td>
+                    <td>Obesidade classe I</td>
                 </tr>
-                <tr>
-                    <td class="imc-obesidade2">35,0 - 39,9</td>
-                    <td class="imc-obesidade2">Obesidade classe II</td>
+                <tr id="imc-obesidadeII">
+                    <td>35,0 - 39,9</td>
+                    <td>Obesidade classe II</td>
                 </tr>
-                <tr>
-                    <td class="imc-obesidade3">Maior ou igual a 40,0</td>
-                    <td class="imc-obesidade3">Obesidade classe III</td>
+                <tr id="imc-obesidadeIII">
+                    <td>Maior ou igual a 40,0</td>
+                    <td>Obesidade classe III</td>
                 </tr>
             </tbody>
         </table>
-        <h1>TODO: a categoria onde o usuário se encaixa terá a cor verde.</h1>
-        <h1>TODO: Adicionar um CSS bonito</h1>
-        <a href="index.html" class="btn-voltar btn btn-primary">Voltar</a>
+
+        <script>
+            <?php
+            echo "\nconst classRow = document.getElementById('$classId');";
+            ?>
+            classRow.classList.add("highlight");
+        </script>
+
+        <!-- <h1>TODO: a categoria onde o usuário se encaixa terá a cor verde.</h1>
+        <h1>TODO: Adicionar um CSS bonito</h1> -->
     </div>
+    <a href="index.html" class="btn-voltar btn btn-primary">Voltar</a>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
